30447e921942d97788e76bd2146debdb8d86cd3e
Bhavani0310 <bhavaniprasad0141@gmail.com>
2025-04-23 11:01:38 +0530

    cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://content-flow-alpha.vercel.app"],
     methods: ["GET", "POST", "OPTIONS","PUT","DELETE"],
     credentials: true,
   })
